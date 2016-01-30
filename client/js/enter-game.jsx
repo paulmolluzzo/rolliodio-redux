dbe58a64c369348d0176ccf006ed780cf7ac389d
Paul Molluzzo <paul@molluzzo.com>
2016-01-29 21:19:42 -0500

    add ability to enter game

--- client/js/enter-game.jsx
+++ client/js/enter-game.jsx
@@ -1,10 +1,29 @@
 EnterGame = React.createClass({
+
+  getInitialState() {
+    return {gameName: ''};
+  },
+
+  targetGame(event) {
+    this.setState({
+      gameName: event.target.value
+    });
+  },
+
+  enterGame(event) {
+    if (Games.findOne({slug: this.state.gameName})) {
+      FlowRouter.go('currentgame', {slug: this.state.gameName})
+    } else {
+      Session.set('alert', {type: 'error', message: 'That game doesn\'t exist'});
+    }
+  },
+
   render() {
     return (
       <div className="enter-game">
         <p>OR</p>
-        <input id="enter-game-name" type="text" placeholder="Join a Live Game!" />
-        <input type="button" className="enter-game" value="»" />
+        <input id="enter-game-name" type="text" placeholder="Join a Live Game!" onChange={this.targetGame} />
+        <input type="button" className="enter-game" value="»" onClick={this.enterGame} />
     </div>
     );
   }
