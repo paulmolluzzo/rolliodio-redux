c6a127c26d41a947acfc08be33920998e093316b
Paul Molluzzo <paul@molluzzo.com>
2016-02-06 22:28:12 -0500

    add in basic logging in; toggle between login/logout

--- client/js/accounts/login-signup-form.jsx
+++ client/js/accounts/login-signup-form.jsx
@@ -14,6 +14,7 @@ LoginSignupForm = React.createClass({
 
   getInitialState() {
     return {
+      doSignup: 'signup',
       username: '',
       password: ''
     };
@@ -22,6 +23,12 @@ LoginSignupForm = React.createClass({
   collectData(event) {
     this.setState({
       [event.target.name]: event.target.value
+    });
+  },
+
+  toggleSignupLogin(event) {
+    this.setState({
+      doSignup: event.target.name
     });
   },
 
@@ -39,13 +46,50 @@ LoginSignupForm = React.createClass({
     );
   },
 
+  handleLogin(event) {
+    event.preventDefault();
+    Meteor.loginWithPassword(
+      this.state.username,
+      this.state.password,
+      (error) => {
+        if (!error) {
+          console.log('Logged In');
+        } else {
+          console.log(error);
+        }
+      }
+    );
+  },
+
   signupForm() {
     return (
       <form className="signup" onSubmit={this.handleSignup} >
-        <input name="username" onChange={this.collectData} />
-        <input type="password" name="password" onChange={this.collectData} />
+        <h2>Sign Up</h2>
+        <input type="text" name="username" value={this.state.username} onChange={this.collectData} />
+        <input type="password" name="password" value={this.state.password} onChange={this.collectData} />
         <input type="submit" />
       </form>
+    )
+  },
+
+  loginForm() {
+    return (
+      <form className="login" onSubmit={this.handleLogin} >
+        <h2>Log In</h2>
+        <input type="text" name="username" value={this.state.username} onChange={this.collectData} />
+        <input type="password" name="password" value={this.state.password} onChange={this.collectData} />
+        <input type="submit" />
+      </form>
+    )
+  },
+
+  signupLoginForms() {
+    return (
+      <div className="forms">
+        <button name="signup" className="choose signup" onClick={this.toggleSignupLogin} >Signup</button>
+        <button name="login" className="choose login" onClick={this.toggleSignupLogin} >Log In</button>
+        {this.state.doSignup === 'signup' ? this.signupForm() : this.loginForm() }
+      </div>
     )
   },
 
@@ -61,7 +105,7 @@ LoginSignupForm = React.createClass({
   render() {
     return (
       <div className="login-signup-block">
-        {this.data.user || Meteor.loggingIn() ? this.signoutAction() : this.signupForm() }
+        {this.data.user || Meteor.loggingIn() ? this.signoutAction() : this.signupLoginForms() }
       </div>
     );
   }
