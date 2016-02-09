54d449520bc7d3797e8530db3e611de79805e57e
Paul Molluzzo <paul@molluzzo.com>
2016-02-08 21:14:09 -0500

    tie permissions to game ownership

--- server/publications.js
+++ server/publications.js
@@ -13,21 +13,15 @@ Meteor.publish('dice', function(currentGame){
 Games.allow({
   insert: function (userId, doc) {
     // the user must be logged in, and the document must be owned by the user
-    // return (userId && doc.owner === userId);
-
-    // return true for everyone
-    return true
+    return (userId && doc.owner === userId);
   },
   update: function (userId, doc, fields, modifier) {
-    // can only change your own documents
-    // return doc.owner === userId;
-
-    // return true for everyone
-    return true
+    // the user must be logged in, and can only change your own games
+    return (userId && doc.owner === userId);
   },
   remove: function (userId, doc) {
-    // can't delete
-    return false;
+    // the user must be logged in, and can only delete your own games
+    return (userId && doc.owner === userId);
   },
 });
 
@@ -48,7 +42,7 @@ Dice.allow({
   },
   remove: function (userId, doc) {
     // can only remove your own documents
-    return doc.owner === userId;
+    // return doc.owner === userId;
 
     // return true for everyone
     return true
