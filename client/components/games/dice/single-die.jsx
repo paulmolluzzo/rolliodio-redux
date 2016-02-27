63eaaed78e49f95129f19b86ee8d574b25821710
Paul Molluzzo <paul@molluzzo.com>
2016-02-26 20:39:03 -0500

    better styling for single dice using current functionality

--- client/components/games/dice/single-die.jsx
+++ client/components/games/dice/single-die.jsx
@@ -34,30 +34,25 @@ SingleDie = React.createClass({
 
   render() {
     return (
-      <div data-id={this.props.die._id} className="row">
-        <div className="card col-xs-12">
-          <div className="card-block" >
+      <div data-id={this.props.die._id} className="row single-die">
+        <div className="card col-xs-12 die-wrap">
+          <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={this.deleteDie}>
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <div className="card-block">
             <form className="form-inline">
               <div className="form-group">
-                <label for="die-count">Count:</label>
-                <input type="text" className="form-control" id="die-count" placeholder={this.props.die.sides} onBlur={this.updateSides} />
+                <label for="die-count" className="sides">Sides:</label>
+                <input id="die-count" className="side-selector" type="text" placeholder={this.props.die.sides} onBlur={this.updateSides} />
               </div>
-              <div className="form-group">
-                <label for="die-sides">Sides:</label>
-                <input type="text" className="form-control" id="die-sides" placeholder={this.props.die.sides} onBlur={this.updateSides} />
-              </div>
-              <div className="form-group">
-                <label for="die-keep">Keep:</label>
-                <input type="text" className="form-control" id="die-keep" placeholder={this.props.die.sides} onBlur={this.updateSides} />
-              </div>
-              <div className="form-group">
-                <label for="die-multiply">Multiply:</label>
-                <input type="text" className="form-control" id="die-multiply" placeholder={this.props.die.sides} onBlur={this.updateSides} />
-              </div>
-              <div className="form-group">
-                <input type="button" className="form-control" className="btn btn-success" value="Roll" />
+              <div className="form-group results">
+                <label className={this.props.die.type}>{this.props.die.result}</label>
               </div>
             </form>
+          </div>
+          <p className="roll-time">As of: {this.props.die.rolled}</p>
+          <div className="card-block">
+            <input type="button" className="btn  btn-block btn-success" value="Roll" onClick={this.rollDie}/>
           </div>
         </div>
       </div>
